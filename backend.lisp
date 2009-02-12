e44f4af428850635200bfe0ce9b63ce814644dc4
Felipe Delgado <felideon+git@gmail.com>
2009-02-12 03:36:35 -0500

    Created sample page to view blog entry titles. Also created scripts to start the Davenblog server.

--- backend.lisp
+++ backend.lisp
@@ -3,6 +3,15 @@
 (defun create-davenblog-db ()
   (create-database "davenblog"))
 
+(defun create-design-doc-all-posts ()
+  (defdesign all-posts
+      ((all-post-titles :map (doc)
+			     (when (string= (@ doc :type) "DAVENBLOG::BLOG-POST")
+			       (emit (@ doc :title) doc))))
+    (:documentation "Returns all blog posts by title.")
+    (:sync davenblog)))
+
+
 (defdoc blog-post
     ((:timestamp :validator #'numberp)
      (:author :validator #'stringp)
@@ -18,3 +27,9 @@
 		     :title title
 		     :body body
 		     :tags tags))
+
+(defmacro get-all-posts ((&key view) &body body)
+  `(couch-request :get (davenblog/_view/all_posts/,view ,@body)))
+
+(defun get-all-posts-by-title ()
+  (get-all-posts (:view all_post_titles)))
--- davenblog.asd
+++ davenblog.asd
@@ -1,4 +1,5 @@
 (asdf:defsystem #:davenblog
   :depends-on (#:cl-couch #:hunchentoot #:cl-who)
   :components ((:file "package")
-	       (:file "backend" :depends-on ("package"))))
+	       (:file "backend" :depends-on ("package"))
+	       (:file "base-pages" :depends-on ("backend"))))
